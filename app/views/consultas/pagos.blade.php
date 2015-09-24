efb0e067b80513cf8a28002ff0221ecba492491c
leonel madrid <leonel.madrid@hotmail.com>
2015-09-23 18:20:16 -0600

    actualizando vistas

--- app/views/consultas/pagos.blade.php
+++ app/views/consultas/pagos.blade.php
@@ -6,16 +6,16 @@ $(document).ready(function()
 
     $('#example').dataTable({
         "aoColumnDefs": [
-            {"sClass": "width10",              "sTitle": "DPI",          "aTargets": [0]},
-            {"sClass": "width15",              "sTitle": "Usuario",      "aTargets": [1]},
-            {"sClass": "width15",              "sTitle": "Cliente",      "aTargets": [2]},
-            {"sClass": "width15",              "sTitle": "Direccion",    "aTargets": [3]},
-            {"sClass": "width10",              "sTitle": "Melonera",    	"aTargets": [4]},
-            {"sClass": "width5",              "sTitle": "Cuadrilla",  	"aTargets": [5]},
-            {"sClass": "width5",              "sTitle": "S. Anterior",  "aTargets": [6]},
+            {"sClass": "width10 pagos",              "sTitle": "DPI",          "aTargets": [0]},
+            {"sClass": "width10 pagos",              "sTitle": "Usuario",      "aTargets": [1]},
+            {"sClass": "width20 pagos",              "sTitle": "Cliente",      "aTargets": [2]},
+            {"sClass": "width15 pagos",              "sTitle": "Direccion",    "aTargets": [3]},
+            {"sClass": "width10 pagos",              "sTitle": "Melonera",    	"aTargets": [4]},
+            {"sClass": "width5  pagos",              "sTitle": "Cuadrilla",  	"aTargets": [5]},
+            {"sClass": "width5",              "sTitle": "Anterior",  "aTargets": [6]},
             {"sClass": "width5",              "sTitle": "Monto",        "aTargets": [7]},
-            {"sClass": "width5",              "sTitle": "S. Actual",  	"aTargets": [8]},
-            {"sClass": "width15",              "sTitle": "Fecha",  		"aTargets": [9]},
+            {"sClass": "width5",              "sTitle": "Actual",  	"aTargets": [8]},
+            {"sClass": "width15 pagos",              "sTitle": "Fecha",  		"aTargets": [9]},
         ],
 
         "fnDrawCallback": function( oSettings ) {
