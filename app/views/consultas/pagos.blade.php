da5c5b0068cc857d50448440e13f154dbe790ad0
leonel madrid <leonel.madrid@hotmail.com>
2015-09-23 18:14:25 -0600

    actualizando vistas

--- app/views/consultas/pagos.blade.php
+++ app/views/consultas/pagos.blade.php
@@ -8,14 +8,14 @@ $(document).ready(function()
         "aoColumnDefs": [
             {"sClass": "width10",              "sTitle": "DPI",          "aTargets": [0]},
             {"sClass": "width15",              "sTitle": "Usuario",      "aTargets": [1]},
-            {"sClass": "width20",              "sTitle": "Cliente",      "aTargets": [2]},
+            {"sClass": "width15",              "sTitle": "Cliente",      "aTargets": [2]},
             {"sClass": "width15",              "sTitle": "Direccion",    "aTargets": [3]},
             {"sClass": "width10",              "sTitle": "Melonera",    	"aTargets": [4]},
             {"sClass": "width5",              "sTitle": "Cuadrilla",  	"aTargets": [5]},
             {"sClass": "width5",              "sTitle": "S. Anterior",  "aTargets": [6]},
             {"sClass": "width5",              "sTitle": "Monto",        "aTargets": [7]},
             {"sClass": "width5",              "sTitle": "S. Actual",  	"aTargets": [8]},
-            {"sClass": "width10",              "sTitle": "Fecha",  		"aTargets": [9]},
+            {"sClass": "width15",              "sTitle": "Fecha",  		"aTargets": [9]},
         ],
 
         "fnDrawCallback": function( oSettings ) {
